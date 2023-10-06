2eed8700d5e8b1b55db4d02c010cdd10d4cd346f
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-10-06 11:40:53 +0600

    I have a upload programming c file

---
+++ main.c
@@ -0,0 +1,72 @@
+
+// Deferents Number Of Sum --------
+
+         #include<stdio.h>
+          int main(){
+
+          int num1,num2,sum;
+          printf("Enter a Number: ");
+          scanf("%d %d",&num1,&num2);
+
+          sum = num1 + num2;
+          printf("The Number is: %d\n",sum);
+
+          return 0;
+       }
+
+
+
+// DEferents Numbers of Minus --------
+
+
+           #include<stdio.h>
+            int main(){
+
+            int num1,num2,result;
+            printf("Enter a Number: ");
+            scanf("%d %d",&num1,&num2);
+
+            result = num1 - num2;
+            printf("The Number is: %d\n",result);
+
+            return 0;
+         }
+
+
+  // Deferents Numbers of Into ---------
+
+
+             #include<stdio.h>
+            int main(){
+
+            int num1,num2,result;
+            printf("Enter a Number: ");
+            scanf("%d %d",&num1,&num2);
+
+            result = num1 * num2;
+            printf("The Number is: %d\n",result);
+
+            return 0;
+
+            }
+
+
+    // Deferents Number of Devition --------
+
+
+              #include<stdio.h>
+            int main(){
+
+            int num1,num2,result;
+            printf("Enter a Number: ");
+            scanf("%d %d",&num1,&num2);
+
+            result = num1 / num2;
+            printf("The Number is: %d\n",result);
+
+            return 0;
+
+            }       
+
+
+            // Ends ---------------------
